1a28e338a0bac4f28e4104b858b1743588b337bf
Sahil-Karanth <1paleking1@gmail.com>
2025-07-09 14:24:55 +0100

    server now initiated/quit by user

--- main.py
+++ main.py
@@ -40,6 +40,22 @@ def get_possible_artists(artist_name):
 
     return artist_data
 
+def get_most_recent_song(artist_id):
+
+    releases = sp.artist_albums(artist_id, album_type='single,album', country='US', limit=50)
+    albums = sorted(releases['items'], key=lambda x: x['release_date'], reverse=True)
+    
+    for album in albums:
+        album_id = album['id']
+        tracks = sp.album_tracks(album_id)
+        if tracks['items']:
+            latest_track = tracks['items'][0]
+            print(f"\nMost Recent Song: {latest_track['name']}")
+            print(f"Spotify URL: {latest_track['external_urls']['spotify']}")
+            return
+    
+    print("No tracks found in latest releases.")
+
 # -----------------------------------------------------------------------------#
 # globals functions
 # -----------------------------------------------------------------------------#
@@ -78,7 +94,7 @@ def add_artist():
     selected_artists.append({'name': name, 'id': artist_id, 'popularity': popularity})
     return redirect(url_for('home'))
 
-@app.route('/send_to_esp32')
+@app.route('/send_to_esp32') # TODO rename this as I'm no longer sending artists to the esp32 just strings to write
 def send_to_esp32():
     print("Sending selected artists to ESP32...")
 
@@ -88,4 +104,16 @@ def send_to_esp32():
     return "Data sent to ESP32!"
 
 if __name__ == '__main__':
-    app.run(port=5000)
+
+    while True:
+        inp = input("Enter command (launch/quit): ").strip().lower()
+        
+        if inp == "launch":
+            app.run(port=5000)
+        elif inp == "quit":
+            print("Exiting program.")
+            break
+        else:
+            print("Unknown command. Please enter 'launch' or 'quit'.")
+            
+
