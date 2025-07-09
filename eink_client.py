591a5390dc27fba1922b6eec1d1ca907baf3f7db
Sahil-Karanth <1paleking1@gmail.com>
2025-07-09 19:12:02 +0100

    inital code for the eink client written

--- eink_client.py
+++ eink_client.py
@@ -1,19 +1,46 @@
 import time
-import json
 from spotipy_setup import *
+from datetime import datetime
+import serial
 
 
 UPDATE_INTERVAL = 5 # ! small for testing
+SERIAL_PORT = 'COM3'  # ! CHANGEME
+BAUD_RATE = 115200
+
+def send_to_esp32(data):
+    with serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=1) as ser:
+        time.sleep(2)  # Wait for ESP32 to reset after serial connection opens
+        ser.write((data + '\n').encode('utf-8'))
+        print("Sent:", data)
+        response = ser.readline().decode('utf-8').strip()
+        print("Response:", response)
+
+
 
 if __name__ == '__main__':
     while True:
-
         print("checking for new songs")
 
-        with open("selected_artists.json", "r") as file:
-            selected_artists_dict = json.load(file)
+        conn = sqlite3.connect(DB_PATH)
+        cursor = conn.cursor()
 
-        for artist in selected_artists_dict:
-            get_most_recent_song()
+        selected_artists = load_selected_artists()
+
+        for artist in selected_artists:
+            latest_song = get_most_recent_song(artist['id'])
+            
+            # if new song, update db and eink
+            if latest_song != artist['most_recent_song']:
+                cursor.execute(
+                """
+                UPDATE selected_artists
+                SET most_recent_song = ?, last_updated = ?
+                WHERE id = ?
+                """, (latest_song, datetime.now().isoformat(), artist['id']))
+
+                # update eink display
+                send_to_esp32(f"{latest_song}|{artist['name']}")
+
 
         time.sleep(UPDATE_INTERVAL)
